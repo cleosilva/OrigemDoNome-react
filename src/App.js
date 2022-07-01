07c754f52977ec3c682164a9f2b0cf6e7216e7f4
cleosilva <cleo_silva1906@hotmail.com>
2022-07-01 10:42:05 -0300

    implementando busca do nome

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ export default function App() {
         Accept: 'application/json'
       }
     }).then((response) => response.json())
-      .then((response) => setContries(json))
+      .then((response) => setCountries(json))
   });
 
   // Função para buscar os nomes
@@ -54,6 +54,20 @@ export default function App() {
     }
     setValid(0);
   }
+
+  async function openCountries() {
+    setValid(data);
+    const vetorCountries = [];
+    for (let country of data.country) {
+      let nameCountry = country.country_id
+      for (let country of countries.countries) {
+        if (nameCountry === country.code) {
+          vetorCountries.push(country.name);
+          setNameCountries(vetorCountries);
+        }
+      }
+    }
+  } 
   
   return (
     <div className="area-search">
@@ -64,6 +78,41 @@ export default function App() {
             <BsSearch size={25} color="FFF" />
           </button>
       </div>
+      {Object.keys(data) > 0 && (
+      <main className='main m-4'>
+          <div className="card shadow-lg">
+            <h5 className="card-header text-center">{data.name}</h5>
+            <div className="card-body p-4">
+              <h5 className="card-title">País com maior probablidade de origem: <p className='text-center mt-2 mb-0 text-uppercase text-success'>{nameCountry}</p></h5>
+              <div className='mb-4 d-flex justify-content-center'>
+                <img src={`${flagCountry}`} width="35" alt="description of image" />
+              </div>
+              <p className="card-text text-center ">A probabilidade é: <span className='fw-bold'>{probabilityCountry.toFixed(4)}</span> ou <span className='fw-bold'>{percentCountry.toFixed(0)}% </span></p>
+              <div className=' d-flex justify-content-center'>
+                <button className="btn btn-success px-4 py-2 text-uppercase" data-bs-toggle="tooltip" data-bs-placement="top" title="3 países mais prováveis"
+                  onClick={openCountries}>Principais países <BsArrowDown size={20} color="FFF"/></button>
+              </div>
+            </div>
+          </div>
+        </main>
+    )}
+    {Object.keys(valid).length > 0 && ( //verifica se há conteúdo no objeto para ser mostrado
+        <div className="card shadow-lg mb-5">
+          <h5 className="card-header text-uppercase">Principais Países:</h5>
+          <div className="card-body countries gap-5 d-flex">
+            <span className='text-center d-block'>
+              {nameCountries.map(country => (
+                <p className='d-flex text-uppercase '>{country}</p>
+              ))}
+            </span>
+            <span className='text-center d-block'>
+              {data.country.map(e => (
+                <p className="card-text mx-auto">{e.probability.toFixed(4)} | {(e.probability.toFixed(2) * 100).toFixed(0)}% </p>
+              ))}
+            </span>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
