aa1354a1df3fd1cbc541f546b4061b3b87579c37
cleosilva <cleo_silva1906@hotmail.com>
2022-07-01 21:30:07 -0300

    refatoração do estilo

--- src/App.js
+++ src/App.js
@@ -78,7 +78,7 @@ export default function App() {
       <h1 className="title mt-4">Origin of Name</h1>
       <div className="input-group mb-3 containerInput">
         <input ref={inputElement} type="text" className="form-control" placeholder="Digite seu nome" value={input} onChange={(event) => setInput(event.target.value)}   />
-        <button type="button" className="buttonSearch btn btn-light bg-color" data-bs-toggle="tooltip" data-bs-placement="top" title="Pesquisar" onClick={handleClick}>
+        <button type="button" className="bg-color button1" data-bs-toggle="tooltip" data-bs-placement="top" title="Pesquisar" onClick={handleClick}>
             <BsSearch size={25} color="FFF" />
         </button>
       </div>
@@ -87,24 +87,24 @@ export default function App() {
       <div class="card-group">
       {Object.keys(data).length > 0 && (
       <main className='main m-4'>
-          <div className="card shadow-lg">
+          <div className="card shadow-lg bg-card">
             <h5 className="card-header text-center bg-header">Nome: {data.name}</h5>
             <div className="card-body p-4">
               <h5 className="card-title">País com maior probablidade de origem: <p className='text-center mt-2 mb-0 text-uppercase text-success'>{nameCountry}</p></h5>
               <div className='mb-4 d-flex justify-content-center'>
                 <img src={`${flagCountry}`} width="35" alt="description of image" />
               </div>
-              <p className="card-text text-center ">A probabilidade é: <span className='fw-bold'>{probabilityCountry.toFixed(4)}</span> ou <span className='fw-bold'>{percentCountry.toFixed(0)}% </span></p>
+              <p className="card-text text-center ">A probabilidade é de <span className='fw-bold'>{percentCountry.toFixed(0)}% </span></p>
               <div className=' d-flex justify-content-center'>
-                <button className="btn bg-color px-4 py-2 text-uppercase text-light" data-bs-toggle="tooltip" data-bs-placement="top" title="3 países mais prováveis"
-                  onClick={openCountries}>Outros países <BsArrowRightSquare size={20} color="FFF"/></button>
+                <button className="btn-fancy" data-bs-toggle="tooltip" data-bs-placement="top" title="3 países mais prováveis"
+                  onClick={openCountries}><span>Outros países</span></button>
               </div>
             </div>
           </div>
         </main>
     )}
     {Object.keys(valid).length > 0 && ( //verifica se há conteúdo no objeto para ser mostrado
-        <div className="card shadow-lg m-4">
+        <div className="card shadow-lg m-4 bg-card">
           <h5 className="card-header text-uppercase bg-header">Principais Países:</h5>
           <div className="card-body countries gap-5 d-flex">
             <span className='text-center d-block'>
@@ -114,7 +114,7 @@ export default function App() {
             </span>
             <span className='text-center d-block'>
               {data.country.map(e => (
-                <p className="card-text mx-auto">{e.probability.toFixed(4)} | {(e.probability.toFixed(2) * 100).toFixed(0)}% </p>
+                <p className="card-text mx-auto">{(e.probability.toFixed(2) * 100).toFixed(0)}% </p>
               ))}
             </span>
           </div>
